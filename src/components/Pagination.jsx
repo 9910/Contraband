d5598a7f8728b09c707cd28021f9449995374613
9910 <webdevmastersensei@gmail.com>
2017-08-02 23:40:08 +0200

    Pagination Added

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import Pagination from 'react-js-pagination';
+
+class PaginationMain extends Component {
+	constructor() {
+		super();
+		this.state = {
+			activePage: 1,
+			numOfMovies: 80,
+			movies: []
+		};
+	}
+
+	componentWillMount() {
+		const BASE_URL = 'https://yts.ag/api/v2/list_movies.json?limit=8&page=';
+
+		fetch(BASE_URL + 1, {method: 'GET'})
+			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					movies: json.data.movies, 
+					numOfMovies: json.data.movie_count
+				});
+			});
+	}
+
+	handleSelect(pageNumber) {
+		console.log('Page Number:', pageNumber);
+		console.log('Movies', this.state.movies);
+		console.log('Movie Count', this.state.numOfMovies);
+		const BASE_URL = 'https://yts.ag/api/v2/list_movies.json?limit=8&page=';
+
+		fetch(BASE_URL + pageNumber, {method: 'GET'})
+			.then(response => response.json())
+			.then(json => {
+				this.setState({movies: json.data.movies});
+			});
+		this.setState({activePage: pageNumber});
+	}
+
+	showMovies() {
+		return (
+			<div>
+				<div className="row">
+					{
+						this.state.movies.slice(0, 4).map(movie => {
+							return (
+								<div key={movie.id} className="col l3 s6">
+								    <div className="card">
+								        <div className="card-image">
+								            <a href={'/movie/' + movie.id}>
+								                <img className="movieCover responsive-img" src={movie.medium_cover_image} />
+								            </a>
+								        </div>
+								    </div>
+								    <h5 className="flow-text">{movie.title}</h5>
+								</div>
+							)
+						})
+					}
+				</div>
+				<div className="row">
+					{
+						this.state.movies.slice(4, 8).map(movie => {
+							return (
+								<div key={movie.id} className="col l3 s6">
+								    <div className="card">
+								        <div className="card-image">
+								            <a href={'/movie/' + movie.id}>
+								                <img className="movieCover responsive-img" src={movie.medium_cover_image} />
+								            </a>
+								        </div>
+								    </div>
+								    <h5 className="flow-text">{movie.title}</h5>
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				{this.showMovies()}
+				<Pagination
+				  activePage={this.state.activePage}
+		          itemsCountPerPage={8}
+		          totalItemsCount={this.state.numOfMovies}
+		          pageRangeDisplayed={5}
+		          onChange={pageNumber => this.handleSelect(pageNumber)}
+				/>
+			</div>
+		)
+	}
+}
+
+export default PaginationMain;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './components/App.jsx';
+import Pagination from './components/Pagination.jsx';
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('search')
+    <Router>
+    	<div>
+	    	<Route exact={true} path="/" component={Pagination} />
+	    	<Route path="/browse" component={App} />
+    	</div>
+    </Router>,
+    document.getElementById('root')
 );
